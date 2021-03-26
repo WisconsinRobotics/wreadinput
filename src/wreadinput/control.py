ebbe5333713886ba9edb3471f1129b17ae1c379c
phantamanta44 <evandalong@gmail.com>
2021-03-25 20:18:02 -0500

    Fix bad varname in control.py

--- src/wreadinput/control.py
+++ src/wreadinput/control.py
@@ -15,7 +15,7 @@ class AxisPublisher(ControlPublisher):
     def __init__(self, device: InputDevice, axis: DeviceAxis, topic: str):
         self._device = device
         self._axis = axis
-        self._pub = rospy.Publisher(str, Float32, queue_size=10)
+        self._pub = rospy.Publisher(topic, Float32, queue_size=10)
     
     def publish(self):
         state = self._device.get_axis(self._axis)
@@ -26,7 +26,7 @@ class KeyPublisher(ControlPublisher):
     def __init__(self, device: InputDevice, key: DeviceKey, topic: str):
         self._device = device
         self._key = key
-        self._pub = rospy.Publisher(str, Bool, queue_size=10)
+        self._pub = rospy.Publisher(topic, Bool, queue_size=10)
     
     def publish(self):
         state = self._device.get_key(self._key)
